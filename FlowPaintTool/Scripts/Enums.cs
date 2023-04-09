92735a89cf47ebc4044802ea37d250b0edb4c471
huwahuwa <42481055+huwahuwa2017@users.noreply.github.com>
2023-04-10 03:21:35 +0900

    Add files via upload

---
+++ FlowPaintTool/Scripts/Enums.cs
@@ -0,0 +1,29 @@
+﻿namespace FlowPaintTool
+{
+    public enum StartMeshLoadMode
+    {
+        Assets,
+        HierarchyGameObject
+    }
+
+    public enum StartTextureLoadMode
+    {
+        Assets,
+        FilePath
+    }
+
+    public enum PaintMode
+    {
+        FlowPaintMode,
+        ColorPaintMode
+    }
+
+    public enum BrushType
+    {
+        Constant,
+        Linear,
+        Smooth,
+        Squared,
+        InverseSquared
+    }
+}
---
+++ FlowPaintTool/Shaders/FlowPaint2.shader
@@ -0,0 +1,25 @@
+﻿Shader "FlowPaintTool2/FlowPaint2"
+{
+	Properties
+	{
+		[NoScaleOffset]
+		_MainTex("Main Texture", 2D) = "white" {}
+	}
+
+	SubShader
+	{
+		Pass
+		{
+			Cull Off
+
+			CGPROGRAM
+
+			#pragma vertex VertexShaderStage_FlowPaint
+			#pragma fragment FragmentShaderStage_FlowPaint
+
+			#include "Paint2.hlsl"
+
+			ENDCG
+		}
+	}
+}
