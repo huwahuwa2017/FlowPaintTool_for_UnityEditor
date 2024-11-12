6c7d64aec3f033742045986fbea203d3ed221814
huwahuwa2017 <huwahuwa314@gmail.com>
2024-11-12 10:40:19 +0900

    v86
    Refactoring

--- FlowPaintTool/Shaders/BlitShader.hlsl
+++ FlowPaintTool/Shaders/BlitShader.hlsl
@@ -26,10 +26,9 @@ static int2 _OffsetArray[4] =
     int2(1, 0), int2(-1, 0), int2(0, 1), int2(0, -1)
 };
 
-uint2 IsOutOfRange(in float2 range, in int2 index, out bool outOfRange)
+bool IsOutOfRange(int2 range, int2 index)
 {
-    outOfRange = index.x < 0 || index.y < 0 || index.x >= range.x || index.y >= range.y;
-    return uint2(index) * (!outOfRange);
+    return index.x < 0 || index.y < 0 || index.x >= range.x || index.y >= range.y;
 }
 
 
@@ -65,9 +64,11 @@ float FragmentShaderStage_FillBleed(V2F input) : SV_Target
     
     for (int count = 0; count < 4; ++count)
     {
-        bool isOutOfRange = false;
-        uint2 index1 = IsOutOfRange(_MainTex_TexelSize.zw, int2(index0) + _OffsetArray[count], isOutOfRange);
-        temp0 = max(temp0, _MainTex[index1].r * (!isOutOfRange));
+        int2 index1 = int2(index0) + _OffsetArray[count];
+        bool isOutOfRange = IsOutOfRange(_MainTex_TexelSize.zw, index1);
+        index1 = isOutOfRange ? 0 : index1;
+        
+        temp0 = max(temp0, isOutOfRange ? 0.0 : _MainTex[index1].r);
     }
     
     return (temp0 > 0.25) ? 0.5 : 0.0;
@@ -114,11 +115,12 @@ float4 FragmentShaderStage_Bleed(V2F input) : SV_Target
     
     for (int count = 0; count < 4; ++count)
     {
-        bool isOutOfRange = false;
-        uint2 index1 = IsOutOfRange(_MainTex_TexelSize.zw, int2(index0) + _OffsetArray[count], isOutOfRange);
+        int2 index1 = int2(index0) + _OffsetArray[count];
+        bool isOutOfRange = IsOutOfRange(_MainTex_TexelSize.zw, index1);
+        index1 = isOutOfRange ? 0 : index1;
         
-        bool flag = (_FillTex[index1].r * (!isOutOfRange)) > 0.75;
-        result += _MainTex[index1] * flag;
+        bool flag = (!isOutOfRange) && (_FillTex[index1].r > 0.75);
+        result += flag ? _MainTex[index1] : 0.0;
         margeCount += flag;
     }
     
